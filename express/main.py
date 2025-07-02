8a90597cda2df6a0e84feaf451030731df02d6bf
NaphatJM <naphatjm.work@gmail.com>
2025-07-02 09:55:40 +0700

    lecture : simple CRUD Concepts

--- express/main.py
+++ express/main.py
@@ -9,22 +9,21 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 
-# this is how we sent data via parameter
 @app.get("/items/{item_id}")
-def read_param(item_id: int):
-    return {"item_id": item_id}
-
-
-# this is how we sen data via query string
-@app.get("/imgs/{img_id}")
-# we use Union for optional value, like it can be null by default
-def read_query(img_id: int, q: Union[str, None] = None):
-    return {"img_id": img_id, "q": q}  # GET: 127.0.0.1:8000/imgs/5/?q=hello
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
 
 
 @app.post("/items")
-# when we use request we have to import Request class and function have to async
-async def post_item(req: Request):
-    body = await req.json()  # json() convert json into dictionary name "body"
-    print(body["name"])  # show in console
-    return {"body": body}  # we return dict name "body"
+async def create_item(item: dict):
+    return {"item": item}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: dict):
+    return {"item_id": item_id, "item": item}
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": f"Item with ID {item_id} has been deleted"}
