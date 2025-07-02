2b77ebdbd2506e1eef5d6a5c7a35744bc67ddd3c
NaphatJM <naphatjm.work@gmail.com>
2025-07-02 21:19:12 +0700

    lecture: schema validation concepts

--- express/main.py
+++ express/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, Request
-from typing import Union
+
+# use pydantic for schema validation
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    cost: float
+    is_offer: bool = False
 
 
 @app.get("/")
@@ -9,19 +17,23 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 
+#  if you watch following api closely you can see that we use Item(Model) that we created instead of dict
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
+async def read_item(item_id: int, q: str | None = None) -> Item:
     return {"item_id": item_id, "q": q}
 
 
 @app.post("/items")
-async def create_item(item: dict):
+async def create_item(item: Item):
     return {"item": item}
 
 
+# for example if you not sent body(json) that match schema via request, its will return 422 Error: Unprocessable Entity
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: dict):
-    return {"item_id": item_id, "item": item}
+async def update_item(item_id: int, item: Item):
+    print(item)
+    # return Item(name=item.name, cost=item.cost, is_offer=item.is_offer)
+    return item
 
 
 @app.delete("/items/{item_id}")
